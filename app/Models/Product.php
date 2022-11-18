3eebc7ad83d8d8a93dd41a049677a2e29e7f1830
Alex <alex.kirpichonak@gmail.com>
2022-11-18 17:00:57 +0300

    Task11: added filtering and sorting logic to Product Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,11 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class Product extends Model
 {
     use HasFactory;
 
+    const PRODUCT_FILTERS = ['name', 'manufacturer', 'category'];
+    const PRODUCT_SORT = ['name', 'manufacturer', 'cost', 'category'];
+    protected QueryBuilder $query;
     public $timestamps = false;
     protected $fillable = [
         'name',
@@ -17,4 +21,11 @@ class Product extends Model
         'cost',
         'category',
     ];
+
+    public function getSelected()
+    {
+        return QueryBuilder::for($this)
+            ->allowedFilters(self::PRODUCT_FILTERS)
+            ->allowedSorts(self::PRODUCT_SORT);
+    }
 }
