ea083ab43d1a2b65e51a598361d4808ef5d80448
Alex <alex.kirpichonak@gmail.com>
2022-11-21 09:03:12 +0300

    Task13: installing laravel/breeze

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\PanelController;
-use App\Http\Controllers\CatalogController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ServiceController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,27 +13,19 @@ use App\Http\Controllers\ServiceController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::resources([
-    'products' => ProductController::class,
-    'services' => ServiceController::class,
-]);
-Route::get('/', [CatalogController::class, 'index']);
-Route::get('/card/{id}', [CatalogController::class, 'card'])->name('catalog.card');
-Route::get('/add-service/{id}', [CatalogController::class, 'addService'])->name('catalog.add-service');
-Route::get('/remove-service/{id}', [CatalogController::class, 'removeService'])->name('catalog.remove-service');
-Route::get('/admin', [PanelController::class, 'index'])->name('admin');
-Route::get('/admin/products', [App\Http\Controllers\Admin\ProductController::class, 'index'])->name('admin.products.index');
-Route::get('admin/services', [App\Http\Controllers\Admin\ServiceController::class, 'index'])->name('admin.services.index');
-//Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-//Route::get('/services/create', [ServiceController::class, 'create'])->name('services.create');
-//Route::get('/product/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
-//Route::get('/service/{id}/edit', [ServiceController::class, 'edit'])->name('services.edit');
 
-//Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-//Route::post('/services', [ServiceController::class, 'store'])->name('services.store');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-//Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.delete');
-//Route::delete('/services/{id}', [ServiceController::class, 'destroy'])->name('services.delete');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-//Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
-//Route::put('/services/{id}', [ServiceController::class, 'update'])->name('services.update');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
